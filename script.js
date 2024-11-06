1d7c3926a65cc2717aad26b481f8e5c7ccccdfc8
shiparezikpro <79908977+shiparezikpro@users.noreply.github.com>
2024-11-06 22:25:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Открытие модального окна
 function openModal() {
     document.getElementById('modal').style.display = 'block';
 }
 
-// Закрытие модального окна
 function closeModal() {
     document.getElementById('modal').style.display = 'none';
 }
 
-// Переход к выбранной главе
 function goToChapter(chapter) {
     window.location.href = chapter;
 }
